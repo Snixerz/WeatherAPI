a346ce3186bc85f56f8371ced432474f1bb4ab17
Snixerz <cakeisew@gmail.com>
2019-11-18 14:25:27 -0800

    Finished Project

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-# WeatherAPI
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/weather')
+def weather():
+    return render_template('weather_form.html')
+
+@app.route('/weather_results')
+def weather_results_page():
+    users_city = request.args.get('city')
+
+    params = {
+        'q': users_city,
+        'appid':"9f87d4a1669aa1f928d4ce146001bdec"
+    }
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather', params=params)
+
+    response_json = response.json()
+    results = response_json.get('main').get('temp')
+    return render_template('weather_results.html', results=results)
